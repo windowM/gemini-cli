4ffecbe713d6f4919e832a20f24138cf2dc8a868
windowm9 <windowm9@kyonggi.ac.kr>
2026-03-19 05:41:48 +0000

    Add Dark/Light mode functionality

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-// Add JS here
+const themeToggle = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'dark') {
+    themeToggle.textContent = 'Switch to Light Mode';
+  }
+}
+
+themeToggle.addEventListener('click', () => {
+  let theme = document.documentElement.getAttribute('data-theme');
+  if (theme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    themeToggle.textContent = 'Switch to Dark Mode';
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    themeToggle.textContent = 'Switch to Light Mode';
+  }
+});
